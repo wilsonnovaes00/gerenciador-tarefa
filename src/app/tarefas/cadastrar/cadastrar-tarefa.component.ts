5800a2113830c19919d01b0b1eaced0bde8875ba
Raquel <raquel.costa@ttwgroup.com>
2017-08-14 17:05:39 -0300

    Define Add tarefa

--- src/app/tarefas/cadastrar/cadastrar-tarefa.component.ts
+++ src/app/tarefas/cadastrar/cadastrar-tarefa.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { TarefaService, Tarefa } from '../shared';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'my-cadastrar-tarefa',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarTarefaComponent implements OnInit {
 
-  constructor() { }
-
+  @ViewChild('formTarefa') formTarefa: NgForm;
+  tarefa: Tarefa;
+  constructor(private tarefaService: TarefaService, private router: Router) { }
   ngOnInit() {
+    this.tarefa = new Tarefa();
   }
-
+  addTarefa(): void {
+    if (this.formTarefa.form.valid) {
+      this.tarefaService.addTarefas(this.tarefa);
+      this.router.navigate(['/tarefas']);
+    }
+  }
 }
