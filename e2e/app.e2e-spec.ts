5cf626c9bd7495cdf0f686df7bda16fc5148e829
Raquel <raquel.costa@ttwgroup.com>
2017-08-14 12:39:53 -0300

    Import bootstrap

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -6,9 +6,4 @@ describe('gerenciador-tarefa App', () => {
   beforeEach(() => {
     page = new GerenciadorTarefaPage();
   });
-
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to my!');
-  });
 });
