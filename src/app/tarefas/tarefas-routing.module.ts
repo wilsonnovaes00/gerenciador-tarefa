f3a977f6e9ca6ff201a9f06cdf546fd3f373c7cd
Raquel <raquel.costa@ttwgroup.com>
2017-08-14 17:57:48 -0300

    add Router edit tarefa

--- src/app/tarefas/tarefas-routing.module.ts
+++ src/app/tarefas/tarefas-routing.module.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ListarTarefaComponent } from './listar';
 import { CadastrarTarefaComponent } from './cadastrar';
+import { EditarTarefaComponent } from './editar';
 
 export const TarefaRoutes: Routes = [
   {
@@ -14,5 +15,9 @@ export const TarefaRoutes: Routes = [
   {
     path: 'tarefas/cadastrar',
     component: CadastrarTarefaComponent
+  },
+  {
+    path: 'tarefas/editar/:id',
+    component: EditarTarefaComponent
   }
 ];
